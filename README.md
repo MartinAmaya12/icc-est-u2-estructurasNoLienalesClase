beba018fed946d8d5c3f849c896956d5cfdcd648
Bryan <b0sry2020xd@gmail.com>
2026-01-05 13:02:07 -0500

    IntTree implementation.

---
+++ README.md
@@ -0,0 +1,46 @@
+# Práctica: Estructuras No Lineales - 
+
+## Autor
+- Nombre: Renato Martin Amaya Siguenza
+- Carrera/Curso: Estructura de Datos
+
+
+##  Nombre de la práctica - Fecha
+- Práctica: Practica de Arboles-Implementacion de Integers
+- Fecha: 05/01/2026
+
+## Descripción
+Objetivo. Implementar un Árbol Binario de Búsqueda para enteros con recorridos PreOrder, InOrder, PostOrder y un método size() que retorne el total de nodos en complejidad O(1).
+
+Entregable. Repositorio con:
+
+Código fuente actualizado.
+
+Evidencias en la carpeta assets/ (o equivalente).
+
+README de la práctica actualizado.
+
+Requisitos
+
+El árbol debe permitir inserción de enteros.
+
+Debe imprimir en consola los tres recorridos:
+
+PreOrder
+
+InOrder
+
+PostOrder
+
+Debe imprimir en consola el tamaño del árbol.
+
+size() debe ser O(1). Para cumplirlo, el árbol debe mantener un contador interno (ej. private int size;) que se actualice en insert (y en delete si existiera).
+
+## Evidencias
+### Captura 1
+Inserta aquí la captura del código o de la ejecución.
+
+
+### Captura 2 
+Inserta aquí una segunda captura si aplica.
+- Archivo: `assets/captura-2.png`
---
+++ src/trees/IntTree.java
@@ -0,0 +1,72 @@
+package trees;
+import nodes.Node;
+public class IntTree {
+   private Node<Integer> root;
+   private int size;
+   
+   public IntTree(){
+    this.root = null;
+    this.size = 0;
+   }
+
+   public void insert(int value){
+    root = insertRecursive(root, value);
+   }
+
+   private Node<Integer> insertRecursive(Node<Integer> current, int value){
+    if(current == null){
+        size++;
+        return new Node<>(value);
+    }
+    if(value < current.getValue()){
+        current.setLeft(insertRecursive(current.getLeft(), value));
+    }else if(value > current.getValue()){
+        current.setRight(insertRecursive(current.getRight(), value));
+    }
+    return current;
+   }
+
+   public void preOrder(){
+    preOrderRecursive(root);
+   }
+
+   private void preOrderRecursive(Node<Integer> node){
+    if(node != null){
+        System.out.println(node.getValue() + " ");
+        preOrderRecursive(node.getLeft());
+        preOrderRecursive(node.getRight());
+    }
+   }
+
+   public void posOrder(){
+    posOrderRecursive(root);
+   }
+
+   private void posOrderRecursive(Node<Integer> node){
+    if(node == null){
+        return;
+    }
+    
+    posOrderRecursive(node.getLeft());
+    posOrderRecursive(node.getRight());
+    System.out.println(node.getValue() + " ");
+   }
+
+   public void inOrder(){
+    inOrderRecursive(root);
+   }
+
+   private void inOrderRecursive(Node<Integer> node){
+    if(node == null){
+    return;
+    }
+    posOrderRecursive(node.getLeft());
+    System.out.println(node.getValue() + " ");
+    posOrderRecursive(node.getRight());
+    }
+
+    public int size(){
+        return size;
+    }    
+
+}
